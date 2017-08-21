a8f0a5c542674a16091f939bb08ead327f02b93d
马雄伟 <maxiongwei@vchangyi.com>
2017-08-21 17:07:15 +0800

    [汽车助手-手机微信]添加其他

--- src/app/router/router.js
+++ src/app/router/router.js
@@ -15,6 +15,9 @@ routes=routes.concat(ComponentsRouter);
 
 let router=new VueRouter({
     routes: routes,
+    scrollBehavior (to, from, savedPosition) {
+        return { x: 0, y: 0 }
+    },
     beforeEach:function(to,from,next){
         window.scrollTo(0,0);
     },
