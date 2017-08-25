ec46315eba992a18310201a789d82d1b77d89c5a
zhangjiayu <zhangjiayu@vchangyi.com>
2017-08-25 17:13:48 +0800

    [汽车助手-手机微信]上传图片

--- src/app/components/demo/demo-jssdk/demo-jssdk.vue
+++ src/app/components/demo/demo-jssdk/demo-jssdk.vue
@@ -44,6 +44,7 @@
                     imgUrl: '', // 分享图标
                     success: function () {
                         // 用户确认分享后执行的回调函数
+                        alert(1)
                     },
                     cancel: function () {
                         // 用户取消分享后执行的回调函数
