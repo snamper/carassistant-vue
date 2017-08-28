335e9f35881657605a1f81d6fd370e2b1e48510a
zhangjiayu <zhangjiayu@vchangyi.com>
2017-08-28 17:25:38 +0800

    [汽车助手-手机微信]文件上传

---
+++ src/app/components/demo/demo-upfiled/demo-upfiled.js
@@ -0,0 +1,11 @@
+import demoUpfiled from './demo-upfiled.vue';
+
+
+let demoUpfiledModule = [
+    { path: '/demo/demo-upfiled', component: demoUpfiled,meta:{title:'demo-upfiled'} },
+]
+demoUpfiledModule=demoUpfiledModule.concat(
+
+);
+
+export default demoUpfiledModule;
