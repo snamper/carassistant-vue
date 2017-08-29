cb3e5c8d48075e1ea04e8eb2ffb455ce9f997ead
zhangjiayu <zhangjiayu@vchangyi.com>
2017-08-29 19:46:57 +0800

    [汽车助手-手信]图片上传 git commit -am [汽车助手-手信]图片上传

--- src/app/main.js
+++ src/app/main.js
@@ -116,7 +116,7 @@ function app_bootstrap(code) {
 
     // 获取用户信息
     wxsdk.config({
-        debug: true,
+        debug: false,
         appId: auth.appid,
         timestamp: auth.timestamp,
         nonceStr: auth.noncestr,
