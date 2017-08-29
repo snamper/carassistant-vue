aaa6f59bcdcc47dc7d49ec677485a8de17726d80
zhangjiayu <zhangjiayu@vchangyi.com>
2017-08-29 11:49:58 +0800

    [汽车助手-手机微信]修改wxsdk配置

--- src/app/plugins/uploader.js
+++ src/app/plugins/uploader.js
@@ -82,15 +82,33 @@ export default {
                     //     status: imageList,
                     //     promise: Q.promise
                     // };
-                    wx.chooseImage({
-                        count: 1, // 默认9
-                        sizeType: ['original', 'compressed'], // 可以指定是原图还是压缩图，默认二者都有
-                        sourceType: ['album', 'camera'], // 可以指定来源是相册还是相机，默认二者都有
-                        success: function (res) {
-                            var localIds = res.localIds; // 返回选定照片的本地ID列表，localId可以作为img标签的src属性显示图片
-                            alert('图片选择成功')
-                        }
-                    });
+                    function chooseImage() {
+                        wx.chooseImage({
+                            count: 1, // 默认9
+                            sizeType: ['original', 'compressed'], // 可以指定是原图还是压缩图，默认二者都有
+                            sourceType: ['album', 'camera'], // 可以指定来源是相册还是相机，默认二者都有
+                            success: function (res) {
+                                var localIds = res.localIds; // 返回选定照片的本地ID列表，localId可以作为img标签的src属性显示图片
+                            }
+                        })
+                        return localIds
+                    }
+                    return new Promise((resolve) => {
+                        wx.chooseImage({
+                            count: 1, // 默认9
+                            sizeType: ['original', 'compressed'], // 可以指定是原图还是压缩图，默认二者都有
+                            sourceType: ['album', 'camera'], // 可以指定来源是相册还是相机，默认二者都有
+                            success: function (res) {
+                                var localIds = res.localIds; // 返回选定照片的本地ID列表，localId可以作为img标签的src属性显示图片
+                                resolve({
+                                    data:res.localIds
+                                })
+                            }
+                        }).then(function (data) {
+                            alert(data)
+                        })
+                    })
+
                 }
             }
         }
