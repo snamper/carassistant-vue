d2d741fe40330fb6ad4219717c0f6d18ec3516cf
zhangjiayu <zhangjiayu@vchangyi.com>
2017-08-30 15:23:19 +0800

    [汽车助手-手机微信]图片上传

--- src/app/components/news/news-list/news-list.vue
+++ src/app/components/news/news-list/news-list.vue
@@ -8,13 +8,13 @@
             <p>文字</p>
         </div>
         <div v-on:click="loadingClick" >点击加载3秒后消失</div>
-        {{promisesss}}22惺惺惜惺惺想寻
+        {{promisesss}}22惺惺惜惺惺想寻00000
         <p>
             <button class='dialog-btn-box' @click='upload()'>点击上传图片</button>
         </p>
-            <!--<div v-for="item in promisesss">-->
-                    <!--<img :src="item" alt="">-->
-            <!--</div>-->
+            <div v-for="item in promisesss">
+                    <img :src="item" alt="">
+            </div>
 
         <div v-listmore="list2">
             <p v-if='list2.hasMore'>加载更多</p>
