c80fa200c56ec5a3515056f52a653914713d729c
zhangjiayu <zhangjiayu@vchangyi.com>
2017-09-01 14:21:19 +0800

    [文件上传]

---
+++ src/app/components/demo/demo-upload/demo-upload.vue
@@ -0,0 +1,64 @@
+
+<template>
+    <div class="demo-upload">
+        demo-upload
+        <div>
+            <div class='upload' v-upfiled='config'>
+                上传文件
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+
+    import uploader from './../../../plugins/uploader.vue'
+    export default {
+        name: 'demo-upfiled',
+        data() {
+            return {
+                config:{
+                    url:'post',
+                    size:'10',
+                    multiple:true,
+                    callback:this.uploadcall
+                },
+            }
+        },
+        created() {
+            //页面创建完成后
+            //自定义标题
+            this.page.setTitle("demo-upfiled");
+        },
+        watch: {
+            //监听动态路由
+        },
+        methods: {
+            //页面方法
+            uploadcall(data){
+                console.log(data)
+            }
+        },
+        components:{
+            uploader
+        }
+    }
+</script>
+
+<style lang="less" scoped>
+    .demo-upload{
+        //页面样式
+        .upload{
+            width: 100%;
+            height: 80/100rem;
+            background: orange;
+            margin-top: 20/100rem;
+            align-items: center;
+            color: #fff;
+            border-radius: 100px;
+            text-align: center;
+            display: flex;
+            justify-content: center;
+        }
+    }
+</style>
