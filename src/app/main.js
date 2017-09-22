8a99833ba658b1451e2d8ff81896928f43cf8776
zhangjiayu <zhangjiayu@vchangyi.com>
2017-09-22 14:56:56 +0800

    [壳牌-汽车助手]ui

--- src/app/main.js
+++ src/app/main.js
@@ -114,7 +114,7 @@ function app_bootstrap(code) {
     var auth = code.jsConfig;
     // 获取用户信息
     wxsdk.config({
-        debug: true,
+        debug: false,
         appId: auth.appId,
         timestamp: auth.timestamp,
         nonceStr: auth.nonceStr,
