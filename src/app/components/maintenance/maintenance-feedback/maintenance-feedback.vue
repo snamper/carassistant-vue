2132bcf2aafc9823e7787532c37deeb37aaf530a
zhangjiayu <zhangjiayu@vchangyi.com>
2017-09-22 15:19:06 +0800

    [壳牌-汽车助手]ui

--- src/app/components/maintenance/maintenance-feedback/maintenance-feedback.vue
+++ src/app/components/maintenance/maintenance-feedback/maintenance-feedback.vue
@@ -22,23 +22,11 @@
                     <span class='title-en font-10 color-gray9'>Image upload</span>
                 </p>
                 <div class='imgbox'>
-                    <div class='item'>
-                            <i class='iconfont icon-quxiaoglyph_ font-12'></i>
-                        <img src="http://img.ivsky.com/img/tupian/slides/201708/14/bailu.jpg" alt="">
+                    <div class='item' v-for="(item,index) in imglist">
+                            <i class='iconfont icon-quxiaoglyph_ font-12' @click='delimg(index)'></i>
+                        <img :src="item.atAttachment" alt="" @click='previewimg(item.atAttachment)'>
                     </div>
-                    <div class='item'>
-                        <i class='iconfont icon-quxiaoglyph_ font-12'></i>
-                        <img src="http://img.ivsky.com/img/tupian/slides/201708/14/bailu.jpg" alt="">
-                    </div>
-                    <div class='item'>
-                        <i class='iconfont icon-quxiaoglyph_ font-12'></i>
-                        <img src="http://img.ivsky.com/img/tupian/slides/201708/14/bailu.jpg" alt="">
-                    </div>
-                    <div class='item'>
-                        <i class='iconfont icon-quxiaoglyph_ font-12'></i>
-                        <img src="http://img.ivsky.com/img/tupian/slides/201708/14/bailu.jpg" alt="">
-                    </div>
-                    <div class='upload'>
+                    <div class='upload' @click='upload()'>
                         <i class='iconfont add_line color-gray9'></i>
                     </div>
                 </div>
@@ -56,6 +44,7 @@
         name: 'maintenance-feedback',
         data() {
             return {
+                imglist:[]
             }
         },
         created() {
@@ -68,6 +57,39 @@
         },
         methods: {
             //页面方法
+            upload(){
+                var self=this;
+                let loading=this.$loading
+                var res=self.uploade({
+                    count: 6-this.imglist.length, // 默认9
+                    sizeType: ['original', 'compressed'], // 可以指定是原图还是压缩图，默认二者都有
+                    sourceType: ['album', 'camera'], // 可以指定来源是相册还是相机，默认二者都有
+                })
+                res.then(function (data) {
+                    //self.imglist=self.imglist.concat(data);
+                    console.log(data)
+                    //alert(self.imglist)
+                    loading.hide()
+                })
+                res.catch(function (err) {
+                    loading.hide()
+                    console.log(err)
+                })
+            },
+            previewimg(atAttachment){
+                debugger
+                var imglist=this.imglist
+                var urlList=[];
+                for (var i in imglist){
+                    urlList.push(imglist[i].atAttachment)
+                }
+                this.imgpreview(atAttachment,urlList)
+            },
+            delimg(index){
+                var self=this;
+                console.log(index)
+                self.imglist.splice(index,1)
+            }
             //反馈
 
         },
