434c42f092d39c89997199a30b3cb1b2c0698a6d
zhangjiayu <zhangjiayu@vchangyi.com>
2017-09-27 11:13:57 +0800

    [壳牌-汽车助手]ui

--- src/app/components/maintenance/maintenance-cycle/maintenance-cycle.vue
+++ src/app/components/maintenance/maintenance-cycle/maintenance-cycle.vue
@@ -123,7 +123,7 @@
                 self.changeType('baoyang')
             }
             setTimeout(function () {
-            //    self.stopDrop()
+                self.stopDrop()
             })
         },
         watch: {
@@ -212,10 +212,10 @@
             },
             stopDrop(){
                 var lastY;//最后一次y坐标点
-                $(document.body).on('touchstart', function(event) {
+                $('.term-right').on('touchstart', function(event) {
                     lastY = event.originalEvent.changedTouches[0].clientY;//点击屏幕时记录最后一次Y度坐标。
                 });
-                $(document.body).on('touchmove', function(event) {
+                $('.term-right').on('touchmove', function(event) {
                     var y = event.originalEvent.changedTouches[0].clientY;
                     var st = $(this).scrollTop(); //滚动条高度 
                     if (y >= lastY && st <= 10) {//如果滚动条高度小于0，可以理解为到顶了，且是下拉情况下，阻止touchmove事件。
