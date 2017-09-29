3d3d160487ce0e8f4d69c310d55633afb0969ac4
zhangjiayu <zhangjiayu@vchangyi.com>
2017-09-29 12:02:16 +0800

    [壳牌-汽车助手]ui

--- src/app/components/maintenance/maintenance-other-recommend/maintenance-other-recommend.vue
+++ src/app/components/maintenance/maintenance-other-recommend/maintenance-other-recommend.vue
@@ -50,25 +50,25 @@
 <script>
     import api from "../../../api/maintenance-api";
     import Vue from 'vue';
-    Vue.directive("zzz",{
-        //  bind: 只调用一次，当指令第一次被绑定到元素时调用。
-        //  inserte: 被绑定元素插入父节点时调用（父节点存在即可调用，不必存在于 document 中）。
-        //  update: 被绑定元素所在的模板更新时调用，而不论绑定值是否变化。通过比较更新前后的绑定值，可以忽略不必要的模板更新。
-        //  componentUpdated: 被绑定元素所在模板完成一次更新周期时调用。
-        //  unbind: 只调用一次，指令与元素解绑时调用。
-        bind(el,binding){
-            setTimeout(
-                function () {
-                    var s=0
-                    var firstHeight=$(el).parent().children()
-                    for(var i=0;i< firstHeight.length;i++){
-                        s=firstHeight.eq(i).innerHeight()+s
-                    }
-                    $(el).parent().height(s)
-                }
-            )
-        },
-    })
+//    Vue.directive("zzz",{
+//        //  bind: 只调用一次，当指令第一次被绑定到元素时调用。
+//        //  inserte: 被绑定元素插入父节点时调用（父节点存在即可调用，不必存在于 document 中）。
+//        //  update: 被绑定元素所在的模板更新时调用，而不论绑定值是否变化。通过比较更新前后的绑定值，可以忽略不必要的模板更新。
+//        //  componentUpdated: 被绑定元素所在模板完成一次更新周期时调用。
+//        //  unbind: 只调用一次，指令与元素解绑时调用。
+//        bind(el,binding){
+//            setTimeout(
+//                function () {
+//                    var s=0
+//                    var firstHeight=$(el).parent().children()
+//                    for(var i=0;i< firstHeight.length;i++){
+//                        s=firstHeight.eq(i).innerHeight()+s
+//                    }
+//                    $(el).parent().height(s)
+//                }
+//            )
+//        },
+//    })
     export default {
         name: 'maintenance-other-recommend',
         data() {
